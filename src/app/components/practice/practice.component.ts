8c57d1a13fd149540d812e7d362fc33c519e8718
Dipak9823 <patildipak363@gmial.com>
2020-03-22 20:45:21 +0530

    ANGULAR SERVICE - use of http-client service

--- src/app/components/practice/practice.component.ts
+++ src/app/components/practice/practice.component.ts
@@ -1,20 +1,32 @@
 import { Component, OnInit } from '@angular/core';
 import { PracticeServiceService } from 'src/app/practice-service/practice-service.service';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-practice',
   templateUrl: './practice.component.html',
   styleUrls: ['./practice.component.scss']
 })
+
 export class PracticeComponent implements OnInit {
 
   private prac;
   StringFromPracticeService :String;
-  constructor(prac:PracticeServiceService) {
-    this.StringFromPracticeService = prac.ServicePractice();
-   }
-
-  ngOnInit() {
+  
+  constructor(prac:PracticeServiceService, private http: HttpClient) {
+  this.StringFromPracticeService = prac.ServicePractice();
   }
 
+  ngOnInit() {
+    let observable = this.http.get("https://api.github.com/users/swapnil1204");
+    observable.subscribe((response) => { console.log(response )});
+  }
+
+  getGitHubUserName(){
+    let observable = this.http.get("https://api.github.com/users/swapnil1204");
+    observable.subscribe((getGitHubUserNameResponse) => { console.log("from getGitHubUserName",getGitHubUserNameResponse)});
+  }
+  
 }
+
+
