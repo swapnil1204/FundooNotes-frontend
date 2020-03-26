da448391659a854c29c589197c0f64a469c28085
Dipak9823 <patildipak363@gmial.com>
2020-03-26 10:02:52 +0530

    home commponent add and new-dashboard component removed

--- src/app/components/home/home.component.spec.ts
+++ src/app/components/home/home.component.spec.ts
@@ -1,20 +1,20 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { NewDashboardComponent } from './new-dashboard.component';
+import { HomeComponent } from './home.component';
 
-describe('NewDashboardComponent', () => {
-  let component: NewDashboardComponent;
-  let fixture: ComponentFixture<NewDashboardComponent>;
+describe('HomeComponent', () => {
+  let component: HomeComponent;
+  let fixture: ComponentFixture<HomeComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ NewDashboardComponent ]
+      declarations: [ HomeComponent ]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(NewDashboardComponent);
+    fixture = TestBed.createComponent(HomeComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
